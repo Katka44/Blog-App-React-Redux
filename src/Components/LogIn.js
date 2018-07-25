a64a174326c7e2d3c40406437ef22887e098458c
Katarina Khan <only4katka@gmail.com>
2018-07-25 14:14:05 +0300

    login page styled

--- src/Components/LogIn.js
+++ src/Components/LogIn.js
@@ -17,7 +17,7 @@ const LogIn = (props) => {
     } = props;
 
     return (
-        <div>
+        <div className="loginWrapperDiv">
             <form className="loginDiv" >
                 <p>{title}</p>
                 <input 
@@ -31,15 +31,30 @@ const LogIn = (props) => {
                     value={password}
                     onChange={handleChange}
                     placeholder="Password"/>
+                <div className="rememberMeDiv">
+                    <input type="checkbox" id="rememberMe"/>
+                    <label for="rememberMe">Remember me</label>
+                </div>
+                
                 <Button   
                    
                     type="button"
                     isLink="false"
                     text={buttonText}
                     handler={handleLogIn} />
+                {signUpLine === "false" ? <p></p> : <p className="smallText">Don't have an account? <span onClick={goToSignUp}>Sign up</span></p>}
             </form>
-            {signUpLine === "false" ? <p></p> : <p className="smallText">Don't have an account? <span onClick={goToSignUp}>Sign up</span></p>}
-            
+            <p className="middle">OR</p>
+            <div className="googleFacebookDiv">
+                <div className="googleDiv">
+                    <i class="fab fa-google"></i>
+                    <p>Log In with Google</p>
+                </div>
+                <div className="facebookDiv">
+                    <i class="fab fa-facebook-f"></i>
+                    <p>Log In with Facebook</p>
+                </div>
+            </div>
         </div>
     );
 }
