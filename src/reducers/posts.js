8a3a66cb5990b7be3a24f2dd718e1c5e6e54d050
Katarina Khan <only4katka@gmail.com>
2018-12-16 15:59:34 +0200

    fixed reducers

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,62 +1,73 @@
 import { posts } from "../data.json";
-import { ADD_POST, DELETE_POST, EDIT_POST, ADD_COMMENT, SORT_POSTS, TOGGLE_LIKE } from "../actions";
+import {
+  ADD_POST,
+  DELETE_POST,
+  EDIT_POST,
+  ADD_COMMENT,
+  SORT_POSTS,
+  TOGGLE_LIKE
+} from "../actions";
 
 const postsReducer = (store = posts, action) => {
-    switch (action.type) {
-        case ADD_POST:
-            return [...store.concat(action.payload)];
+  switch (action.type) {
+    case ADD_POST:
+      return [...store.concat(action.payload)];
 
-        case EDIT_POST:
-            const newObject = 
-                {
-                    id: action.payload.id,
-                    title: action.payload.title,
-                    category: action.payload.category,
-                    headline: action.payload.headline,
-                    subtitle: action.payload.subtitle,
-                    punchline: action.payload.punchline,
-                    textBeginning: action.payload.textBeginning,
-                    textEnd: action.payload.textEnd,
-                    image: action.payload.image,
-                    alt: action.payload.alt,
-                    isLiked: action.payload.isLiked,
-                    likedBy: action.payload.likedBy,
-                    comments: action.payload.comments,
-                    author: action.payload.author,
-                    writtenDate: action.payload.writtenDate
-                };
-            const newStore = [...store];
-            newStore[action.payload.index] = newObject;
-            return newStore;
+    case EDIT_POST:
+      const newObject = {
+        id: action.payload.id,
+        title: action.payload.title,
+        category: action.payload.category,
+        headline: action.payload.headline,
+        subtitle: action.payload.subtitle,
+        punchline: action.payload.punchline,
+        textBeginning: action.payload.textBeginning,
+        textEnd: action.payload.textEnd,
+        image: action.payload.image,
+        alt: action.payload.alt,
+        isLiked: action.payload.isLiked,
+        likedBy: action.payload.likedBy,
+        comments: action.payload.comments,
+        author: action.payload.author,
+        writtenDate: action.payload.writtenDate
+      };
+      const newStore = [...store];
+      newStore[action.payload.index] = newObject;
+      return newStore;
 
-        case DELETE_POST:
-            const newData = [...store];
-            newData.splice(action.payload.index, 1);
-            return newData;
+    case DELETE_POST:
+      const newData = [...store];
+      newData.splice(action.payload.index, 1);
+      return newData;
 
-        case ADD_COMMENT:
-            const newArr = [...store];
-            const newComment = {author: action.payload.author, text: action.payload.text}
-            newArr[action.payload.index].comments.push(newComment);
-            return newArr;
+    case ADD_COMMENT:
+      const newArr = [...store];
+      const newComment = {
+        author: action.payload.author,
+        text: action.payload.text
+      };
+      newArr[action.payload.index].comments.push(newComment);
+      return newArr;
 
-        case TOGGLE_LIKE:
-            const newArray = [...store];
-            const index = newArray[action.payload.index].likedBy.indexOf(action.payload.name);
-            if (index === -1) {
-                newArray[action.payload.index].likedBy.push(action.payload.name)
-            } else {
-                newArray[action.payload.index].likedBy.splice(index, 1)
-            }
-            return newArray;
+    case TOGGLE_LIKE:
+      const newArray = [...store];
+      const index = newArray[action.payload.index].likedBy.indexOf(
+        action.payload.name
+      );
+      if (index === -1) {
+        newArray[action.payload.index].likedBy.push(action.payload.name);
+      } else {
+        newArray[action.payload.index].likedBy.splice(index, 1);
+      }
+      return newArray;
 
-        case SORT_POSTS:
-            const newSt = [...action.payload];
-            return newSt;
+    case SORT_POSTS:
+      const newSt = [...action.payload];
+      return newSt;
 
-        default:
-            return store;
-    }
+    default:
+      return store;
+  }
 };
 
 export default postsReducer;
--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,34 +1,37 @@
 import { users, loggedIn } from "../data.json";
 import { LOG_IN, LOG_OUT, SIGN_UP } from "../actions";
 
-const usersReducer = (store = {users, loggedIn}, action) => {
-    switch (action.type) {
-        case LOG_IN:
-            const newUsers = [...store.users];
-            const checkLogin = () => {
-                return newUsers.map((object, index) => {
-                    return (object.username === action.payload.username && object.password === action.payload.password);
-                });
-            };
-            if (checkLogin().includes(true)) {
-                return {users: newUsers, loggedIn: action.payload.username};
-            } else {
-                window.alert("Username and password do not match");
-            }
-            return store;
+const usersReducer = (store = { users, loggedIn }, action) => {
+  switch (action.type) {
+    case LOG_IN:
+      const newUsers = [...store.users];
+      const checkLogin = () => {
+        return newUsers.map((object, index) => {
+          return (
+            object.username === action.payload.username &&
+            object.password === action.payload.password
+          );
+        });
+      };
+      if (checkLogin().includes(true)) {
+        return { users: newUsers, loggedIn: action.payload.username };
+      } else {
+        window.alert("Username and password do not match");
+      }
+      return store;
 
-        case LOG_OUT:
-            const newArray = [...store.users];
-            return {users: newArray, loggedIn: "null"};
+    case LOG_OUT:
+      const newArray = [...store.users];
+      return { users: newArray, loggedIn: "null" };
 
-        case SIGN_UP:
-            const newArr = [...store.users].concat(action.payload);
-            const result = {users: newArr, loggedIn: "null"};
-            return result;
+    case SIGN_UP:
+      const newArr = [...store.users].concat(action.payload);
+      const result = { users: newArr, loggedIn: "null" };
+      return result;
 
-        default:
-            return store;
-    }
+    default:
+      return store;
+  }
 };
 
-export  default usersReducer;
+export default usersReducer;
